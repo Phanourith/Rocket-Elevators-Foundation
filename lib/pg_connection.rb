f46be6770c515652af977b527c6368e55f3186b3
Phanourith <phanourithkeosavang@gmail.com>
2021-12-03 10:44:53 -0500

    deploy

--- lib/pg_connection.rb
+++ lib/pg_connection.rb
@@ -1,21 +1,21 @@
-module PgConnection
-    class Connection
-        def initialize  
-            @connection = PG::Connection.new(host: ENV["POSTGRESQL_DATABASE_HOST"], port: "5432", dbname: ENV["POSTGRESQL_DATABASE_DBNAME"], user: ENV["POSTGRESQL_DATABASE_USER"], password: ENV["POSTGRESQL_DATABASE_PASSWORD"])
-        end
+# module PgConnection
+#     class Connection
+#         def initialize  
+#             @connection = PG::Connection.new(host: ENV["POSTGRESQL_DATABASE_HOST"], port: "5432", dbname: ENV["POSTGRESQL_DATABASE_DBNAME"], user: ENV["POSTGRESQL_DATABASE_USER"], password: ENV["POSTGRESQL_DATABASE_PASSWORD"])
+#         end
 
-        def execute(statement)
-            @connection.exec(statement)
-        end
+#         def execute(statement)
+#             @connection.exec(statement)
+#         end
 
-        def dbname
-            @connection.db
-        end
+#         def dbname
+#             @connection.db
+#         end
 
 
-        attr_reader :connection
-    end
+#         attr_reader :connection
+#     end
 
-    MyConnection = Connection.new
-end
+#     MyConnection = Connection.new
+# end
 
